25472cc3e62bf07f7c8bcc16ab5deaf1e6f24606
nguyencongminh02 <congminhlctu@gmail.com>
2022-04-13 14:23:38 +0700

    Baithuchanh

---
+++ Baithuchanh1304/src/Shapes/HinhTron.java
@@ -0,0 +1,21 @@
+package Shapes;
+
+import java.util.Scanner;
+
+public class HinhTron extends HinhHoc {
+  public float banKinh;
+  public HinhTron(){
+    ten ="Hinh Tron";
+  }
+  public void nhapBanKinh(){
+    System.out.println("Nhap ban kinh: ");
+    Scanner scanner = new Scanner(System.in);
+    banKinh = scanner.nextFloat();
+  }
+  public void tinhChuVi(){
+    chuVi = 2 * PI * banKinh;
+  }
+  public void tinhDienTich(){
+    dienTich = PI * banKinh * banKinh;
+  }
+}
---
+++ Baithuchanh1304/src/Shapes/HinhTru.java
@@ -0,0 +1,20 @@
+package Shapes;
+
+import java.util.Scanner;
+
+public class HinhTru extends HinhTron {
+  public float chieuCao;
+  public HinhTru(){
+    ten = "Hinh Tru";
+  }
+  public void nhapChieuCao(){
+    nhapBanKinh();
+    System.out.println("Chieu cao = ");
+    Scanner scanner = new Scanner(System.in);
+    chieuCao=scanner.nextFloat();
+  }
+  public void tinhTheTich(){
+    tinhDienTich();
+    theTich = dienTich * chieuCao;
+  }
+}
